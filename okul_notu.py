63de67efffcea516afd8483c30a199bf3fadd1aa
Ahmet Selim <ahmet.selim.chetin@gmail.com>
2023-07-25 12:51:03 +0300

    Almanca dersi eklendi

--- okul_notu.py
+++ okul_notu.py
@@ -6,14 +6,15 @@ not5 = int(input("Din kültürü sınavı notunuzu giriniz :"))
 not6 = int(input("Matematik sınavı notunuzu giriniz :"))
 not7 = int(input("Görsel sanatlar sınavı notunuzu giriniz :"))
 not8 = int(input("Beden eğitimi sınavı notunuzu giriniz :"))
+not9 = int(input("Almanca sınavı notunuzu giriniz :"))
+ortalama = float((not1 + not2 + not3 + not4 + not5 + not6 + not7 + not8 + not9)/9)
+ortalaman = str((not1 + not2 + not3 + not4 + not5 + not6 + not7 + not8 + not9)/9)
 
-ortalama = float((not1 + not2 + not3 + not4 + not5 + not6 + not7 + not8)/8)
-ortalaman = str((not1 + not2 + not3 + not4 + not5 + not6 + not7 + not8)/8)
 
 print(str("Senin ortalaman = " + ortalaman))
 if(ortalama>85.0):
-    print("Takdir belgesi aldın. Aferin sana")
+    print("Takdir belgesi aldın. Aferin sana.")
 elif(ortalama>75.0):
-    print("Teşekkür belgesi aldın. Bir dahakine daha çok çalış")
+    print("Teşekkür belgesi aldın. Bir dahakine daha çok çalış.")
 else:
-    print("Belge alamadın. Üzülme bir dahakine çok daha fazla çalış")
+    print("Belge alamadın. Üzülme bir dahakine çok daha fazla çalış.")
